84297b0b5e9232593c029256fb62312e136b4e78
tckeijner <tckeijner@gmail.com>
2022-01-21 19:14:16 +0100

    user passwords are now encrypted

--- server/store.js
+++ server/store.js
@@ -1,10 +1,22 @@
 const knex = require('knex')(require('./knexfile'));
+const crypto = require('crypto');
 
 module.exports = {
+  encryptPassword,
   registerUser({ email, password }) {
     return knex('user').insert({
       email,
-      password,
+      salt,
+      encrypted_password: encryptPassword(password),
     });
   },
 };
+
+function encryptPassword(password) {
+  const salt = crypto.randomBytes(4).toString('hex');
+  const hash = crypto.createHmac('sha512', salt).update(password);
+  return {
+    salt,
+    hash: hash.digest('hex'),
+  };
+}

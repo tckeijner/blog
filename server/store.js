1f305d317f2cf7820172689999fa19e3b5325487
tckeijner <tckeijner@gmail.com>
2022-01-21 18:55:17 +0100

    move resolve to registeruser route

--- server/store.js
+++ server/store.js
@@ -2,15 +2,9 @@ const knex = require('knex')(require('./knexfile'));
 
 module.exports = {
   registerUser({ email, password }) {
-    return knex('user')
-      .insert({
-        email,
-        password,
-      })
-      .then(() => {
-        console.log('Add user');
-        console.log('email: ' + email);
-        console.log('password: ' + password);
-      });
+    return knex('user').insert({
+      email,
+      password,
+    });
   },
 };

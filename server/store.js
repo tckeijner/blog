a5cb82ed781e8fa1bf8d36ba1b33d2374af7c393
tckeijner <tckeijner@gmail.com>
2022-01-21 19:59:10 +0100

    login function on server

--- server/store.js
+++ server/store.js
@@ -3,20 +3,39 @@ const crypto = require('crypto');
 
 module.exports = {
   encryptPassword,
+
   registerUser({ email, password }) {
+    const { salt, hash } = encryptPassword({ password });
     return knex('user').insert({
       email,
       salt,
-      encrypted_password: encryptPassword(password),
+      encrypted_password: hash,
     });
+  },
+
+  authenticate({ email, password }) {
+    console.log(`Authenticating user ${email}`);
+    return knex('user')
+      .where({ email })
+      .then(([user]) => {
+        if (!user) return { success: false };
+        const { hash } = encryptPassword({
+          password,
+          salt: user.salt,
+        });
+        return { success: hash === user.encrypted_password };
+      });
   },
 };
 
-function encryptPassword(password) {
-  const salt = crypto.randomBytes(4).toString('hex');
+function encryptPassword({ password, salt = randomString() }) {
   const hash = crypto.createHmac('sha512', salt).update(password);
   return {
     salt,
     hash: hash.digest('hex'),
   };
+}
+
+function randomString() {
+  return crypto.randomBytes(4).toString('hex');
 }

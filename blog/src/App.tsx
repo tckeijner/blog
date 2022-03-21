0802845b89f92945587f49b4d9a665cea8c57405
tckeijner <tckeijner@gmail.com>
2022-03-21 22:16:40 +0100

    add redux-persist

--- blog/src/App.tsx
+++ blog/src/App.tsx
@@ -2,7 +2,7 @@ import { theme, useStyles } from './theme/theme';
 import { ThemeProvider } from '@mui/material/styles';
 import EditPost from './Posts/Edit/EditPost';
 import { Provider } from 'react-redux';
-import store from './state/store';
+import store, { persistor } from './state/store';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Home/Home';
 import Posts from './Posts/Published/PublishedPosts';
@@ -10,28 +10,31 @@ import NavBar from './Home/NavBar';
 import RegisterUser from './User/Register';
 import Login from './User/Login';
 import { StyledEngineProvider } from '@mui/material/styles';
+import { PersistGate } from 'redux-persist/integration/react';
 
 function App() {
   const classes = useStyles();
 
   return (
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <StyledEngineProvider injectFirst>
-          <BrowserRouter>
-            <NavBar></NavBar>
-            <div className={classes.app}>
-              <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='new-post' element={<EditPost />} />
-                <Route path='posts' element={<Posts />} />
-                <Route path='register' element={<RegisterUser />} />
-                <Route path='login' element={<Login />} />
-              </Routes>
-            </div>
-          </BrowserRouter>
-        </StyledEngineProvider>
-      </ThemeProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider theme={theme}>
+          <StyledEngineProvider injectFirst>
+            <BrowserRouter>
+              <NavBar></NavBar>
+              <div className={classes.app}>
+                <Routes>
+                  <Route path='/' element={<Home />} />
+                  <Route path='new-post' element={<EditPost />} />
+                  <Route path='posts' element={<Posts />} />
+                  <Route path='register' element={<RegisterUser />} />
+                  <Route path='login' element={<Login />} />
+                </Routes>
+              </div>
+            </BrowserRouter>
+          </StyledEngineProvider>
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   );
 }
--- blog/src/state/store.ts
+++ blog/src/state/store.ts
@@ -1,9 +1,31 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { rootReducer } from './reducers';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+/**
+ * redux-persist will keep the redux store synced with local storage.
+ */
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      // Normally, all data in storage needs to be serializable.
+      // For persist operations it is acceptable and required to make an exception:
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    })
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
